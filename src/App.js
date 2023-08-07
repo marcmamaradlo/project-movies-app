9525e32882de5477816669e7c7ba1db39a7bd475
marcmamaradlo <marc.edwin.mamaradlo@gmail.com>
2023-08-07 13:31:07 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Main from './components/body/Main';
 import Footer from './components/footer/Footer'
 import DynamicPage from './components/main-dynamic-page/Dynamic-Page';
-import { MyContext } from './context';
+// import { MyContext } from './context';
 // import DynamicPage from './components/main-dynamic-page/Dynamic-Page';
 
 const App = () => {
 
-  const context = useContext(MyContext);
-  const state = context.state;
+  // const context = useContext(MyContext);
+  // const state = context.state;
 
   return (
     <>
