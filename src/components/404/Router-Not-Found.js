2933fd27eca0c84356083e07b1e9c02ea42c9c9a
marcmamaradlo <marc.edwin.mamaradlo@gmail.com>
2023-10-28 17:30:01 +0800

    Update Router-Not-Found.js

--- src/components/404/Router-Not-Found.js
+++ src/components/404/Router-Not-Found.js
@@ -1,10 +1,10 @@
 const RouteNotFound = () => {
-    return (
-        <>
-            <h1>404</h1>
-            <h3>Sorry, Nothing found.</h3>
-        </>
-    )
+  return (
+    <>
+      <h1>404</h1>
+      <h3>Sorry, Nothing found.</h3>
+    </>
+  )
 }
 
 export default RouteNotFound;
