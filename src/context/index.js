2b375e4e30aa5467cbd09651deb470c2ffcf71a8
Marc Mamaradlo <marc.edwin.mamaradlo@gmail.com>
2023-11-19 19:49:24 +0800

    Fixes
    Trying to fix issue with localStorage

--- src/context/index.js
+++ src/context/index.js
@@ -1,65 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 const MyContext = React.createContext();
 
 class MyProvider extends Component {
-
   state = {
-    pageID: '',
-    pageType: 'movie', // 'movie', 'tv', 'person',
-    activePage: 'movie', // 'movie', 'tv', 'person', 'search'
+    pageID: "",
+    pageType: "movie", // 'movie', 'tv', 'person',
+    activePage: "movie", // 'movie', 'tv', 'person', 'search'
     hoverEffect: false,
-    apiKey: '0b6d2ddf9c5e096294fa3534fb357915',
+    apiKey: "0b6d2ddf9c5e096294fa3534fb357915",
     // displayComponent: 'main',
-    bgSrc: 'https://images.unsplash.com/photo-1454117096348-e4abbeba002c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    bgSrc:
+      "https://images.unsplash.com/photo-1454117096348-e4abbeba002c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
     apiData: [],
-    recentTrailer: 'day',
+    recentTrailer: "day",
     // comingSoon: 'movie',
     // popular: 'movie',
-    popOutWindow: 'notActive',
-    popOutChannel: 'movie',
-    popOutWindowId: '',
-    carouselData: 'movie',
-    videoID: '', // movie ID in dynamic-page[Dynamic-Banner] '614479'
-    youtubeComponentID: '',
+    popOutWindow: "notActive",
+    popOutChannel: "movie",
+    popOutWindowId: "",
+    carouselData: "movie",
+    videoID: "", // movie ID in dynamic-page[Dynamic-Banner] '614479'
+    youtubeComponentID: "",
     youtubePopOut: false, // true = display: none; || false = display: block;
-    dynamicPageDataID: '',
-    dynamicPageData: '', //'watch-now'
-    dynamicPageDataType: 'movie', // 'tv'
-    viewAllDataName: '', // 'trending', 'popular', 'similar, '',
-    serverButtonID: 'server01', // server01, server02, server03, server04
+    dynamicPageDataID: "",
+    dynamicPageData: "", //'watch-now'
+    dynamicPageDataType: "movie", // 'tv'
+    viewAllDataName: "", // 'trending', 'popular', 'similar, '',
+    serverButtonID: "server01", // server01, server02, server03, server04
     hamburgerIcon: false, // true = display: none; || false = display: block;
     searchButton: false, // true = display: none; || false = display: block;
     searchResult: true, // true = display: flex; || false = display: none;
-    userInput: '',
-    dropDownMenu: 'movie', // 'movie', 'tv', 'people',
-    tvSeason: '', // 0, 1, 2, 3, ...
-    tvEpisode: '', // 0, 1, 2, 3, ...
+    userInput: "",
+    dropDownMenu: "movie", // 'movie', 'tv', 'people',
+    tvSeason: "", // 0, 1, 2, 3, ...
+    tvEpisode: "", // 0, 1, 2, 3, ...
     seriesPlayer: false,
-    filterMenu: 'movie', // 'movie', 'tv', 'person', 'collection', 'company',
+    filterMenu: "movie", // 'movie', 'tv', 'person', 'collection', 'company',
     filteredMovies: 0,
     filteredtv: 0,
     filteredPerson: 0,
     filteredCollection: 0,
-    customButton: 'Custom Button Action',
-    customCardHorizontal: 'Custom Card Horizontal',
+    customButton: "Custom Button Action",
+    customCardHorizontal: "Custom Card Horizontal",
     pageHistory: [],
-  }
+  };
 
-  // handlePageHistory = (e) => {
-  //   const pageHistory = this.state.pageHistory;
-  //   if (pageHistory.length < 5) {
-  //     pageHistory.push(e);
-  //     localStorage.setItem('pageHistory', JSON.stringify(pageHistory));
-  //     console.log(pageHistory);
-  //   } else {
-  //     console.log('History.Length > 20');
-  //   }
-  // }
+  handlePageHistory = (e) => {
+    const pageHistory = this.state.pageHistory;
+    if (pageHistory.length < 5) {
+      pageHistory.push(e);
+      localStorage.setItem("pageHistory", JSON.stringify(pageHistory));
+      console.log(pageHistory);
+    } else {
+      console.log("History.Length > 20");
+    }
+  };
 
   showItemFilterMenu = (e) => {
     this.setState({ filterMenu: e });
-  }
+  };
 
   headingNavigationButton = (a, b, c, d) => {
     this.setState({
@@ -68,25 +68,26 @@ class MyProvider extends Component {
       filteredPerson: c,
       filteredCollection: d,
     });
-  }
+  };
 
-  handleOnBlurEvent = () => { // close the quick search results
+  handleOnBlurEvent = () => {
+    // close the quick search results
     if (this.state.searchResult === true) {
       this.setState({ searchResult: false });
     }
-  }
+  };
 
   handleSearchMenu = (e) => {
     this.setState({ filterMenu: e.target.name });
-  }
+  };
 
   handleNavbarMenu = (e) => {
     this.setState({ activePage: e.target.name });
-  }
+  };
 
   setDynamicPageData = () => {
-    this.setState({ dynamicPageData: 'watchNow' });
-  }
+    this.setState({ dynamicPageData: "watchNow" });
+  };
 
   handleShowEpisode = (seasonIndex, episodeIndex) => {
     this.setState({
@@ -95,149 +96,183 @@ class MyProvider extends Component {
       seriesPlayer: true,
     });
     console.log(seasonIndex, episodeIndex);
-  }
+  };
 
   handleDropDownMenu = (e) => {
     this.setState({ dropDownMenu: e.target.value });
-  }
+  };
 
   handleUserInput = (e) => {
     this.setState({
       userInput: e.target.value,
-      searchResult: true
+      searchResult: true,
     });
-  }
+  };
 
   handleSearchButton = () => {
     this.setState({ searchButton: !this.state.searchButton });
-  }
+  };
 
   handleHamburgerIcon = () => {
     const hamburgerIcon = this.state.hamburgerIcon;
-    this.setState({ hamburgerIcon: !hamburgerIcon, });
-  }
+    this.setState({ hamburgerIcon: !hamburgerIcon });
+  };
 
   handleNavbarMenuButtons = (e) => {
     this.handleHamburgerIcon();
     this.setState({ activePage: e.target.name });
-  }
+  };
 
   handleServerButton = (e) => {
     this.setState({ serverButtonID: e.target.id });
-  }
+  };
 
   handleFakeLinks = (e) => {
     e.preventDefault();
-    alert('Kalma lang, hindi pa tapos yung Website!');
-  }
+    alert("Kalma lang, hindi pa tapos yung Website!");
+  };
 
-  handleTVDynamicButton = (e) => {
-
-  }
+  handleTVDynamicButton = (e) => {};
 
   handleDynamicContentButton = (e) => {
-    if (e.target.name === 'trailer') {
+    if (e.target.name === "trailer") {
       this.setState({ youtubePopOut: true });
     }
     this.setState({
       dynamicPageData: e.target.name,
       youtubeComponentID: e.target.id,
     });
-  }
+  };
 
   handlePopOutTrailerButton = (e) => {
     this.setState({
       dynamicPageDataID: e.target.id,
-      dynamicPageData: 'trailer',
-      popOutWindow: 'notActive',
+      dynamicPageData: "trailer",
+      popOutWindow: "notActive",
       searchResult: false,
       pageID: e.target.id,
       pageType: e.target.name,
     });
-  }
+  };
 
   handlePopOutWatchNowButton = (e) => {
     this.setState({
       dynamicPageDataID: e.target.name,
-      dynamicPageData: 'watch-now',
-      popOutWindow: 'notActive',
+      dynamicPageData: "watch-now",
+      popOutWindow: "notActive",
     });
-  }
+  };
 
   handleYoutubeComponent = (e) => {
-    console.log('clicked!!', e.target.id);
+    console.log("clicked!!", e.target.id);
     this.setState({
       youtubeComponentID: e.target.id,
-      youtubePopOut: true
+      youtubePopOut: true,
     });
-  }
+  };
 
   handleDisplayComponent = (e) => {
-    this.setState({ displayComponent: e.target.id })
-  }
+    this.setState({ displayComponent: e.target.id });
+  };
 
   handlePopOutWindow = (e) => {
     this.setState({
-      popOutWindow: 'active',
+      popOutWindow: "active",
       popOutWindowId: e.target.id,
       popOutChannel: e.target.name,
     });
-  }
+  };
 
   handleClosePopOutWindow = () => {
     this.setState({
-      popOutWindow: 'notActive',
+      popOutWindow: "notActive",
       youtubePopOut: false,
       searchResult: false,
     });
-  }
+  };
 
   handleMouseEnter = (e) => {
     this.setState({
       hoverEffect: true,
       bgSrc: e.target.currentSrc,
     });
-  }
+  };
 
   handleMouseLeave = (e) => {
     this.setState({
       hoverEffect: false,
       bgSrc: e.target.currentSrc,
     });
-  }
-
+  };
 
   render() {
-
     const { pageID, bgSrc, apiData, apiKey } = this.state;
 
-    const
-      {
-        state,
-        handleMouseEnter, handleMouseLeave, handleClosePopOutWindow,
-        handlePopOutWindow, handleDisplayComponent, handleYoutubeComponent,
-        handlePopOutTrailerButton, handlePopOutWatchNowButton, handleDynamicContentButton,
-        handleFakeLinks, handleServerButton, handleHamburgerIcon, handleSearchButton,
-        handleUserInput, handleDropDownMenu, handleShowEpisode, handleNavbarMenu,
-        handleSearchMenu, handleOnBlurEvent, setDynamicPageData, headingNavigationButton,
-        showItemFilterMenu, handleNavbarMenuButtons,
-      } = this;
+    const {
+      state,
+      handleMouseEnter,
+      handleMouseLeave,
+      handleClosePopOutWindow,
+      handlePopOutWindow,
+      handleDisplayComponent,
+      handleYoutubeComponent,
+      handlePopOutTrailerButton,
+      handlePopOutWatchNowButton,
+      handleDynamicContentButton,
+      handleFakeLinks,
+      handleServerButton,
+      handleHamburgerIcon,
+      handleSearchButton,
+      handleUserInput,
+      handleDropDownMenu,
+      handleShowEpisode,
+      handleNavbarMenu,
+      handleSearchMenu,
+      handleOnBlurEvent,
+      setDynamicPageData,
+      headingNavigationButton,
+      showItemFilterMenu,
+      handleNavbarMenuButtons,
+      handlePageHistory,
+    } = this;
 
     return (
       <MyContext.Provider
         value={{
-          state, pageID, bgSrc, apiData, apiKey,
-          handleMouseEnter, handleMouseLeave, handleClosePopOutWindow,
-          handlePopOutWindow, handleDisplayComponent, handleYoutubeComponent,
-          handlePopOutTrailerButton, handlePopOutWatchNowButton, handleDynamicContentButton,
-          handleFakeLinks, handleServerButton, handleHamburgerIcon, handleSearchButton,
-          handleUserInput, handleDropDownMenu, handleShowEpisode, handleNavbarMenu,
-          handleSearchMenu, handleOnBlurEvent, setDynamicPageData, headingNavigationButton,
-          showItemFilterMenu, handleNavbarMenuButtons,
-        }}>
+          state,
+          pageID,
+          bgSrc,
+          apiData,
+          apiKey,
+          handleMouseEnter,
+          handleMouseLeave,
+          handleClosePopOutWindow,
+          handlePopOutWindow,
+          handleDisplayComponent,
+          handleYoutubeComponent,
+          handlePopOutTrailerButton,
+          handlePopOutWatchNowButton,
+          handleDynamicContentButton,
+          handleFakeLinks,
+          handleServerButton,
+          handleHamburgerIcon,
+          handleSearchButton,
+          handleUserInput,
+          handleDropDownMenu,
+          handleShowEpisode,
+          handleNavbarMenu,
+          handleSearchMenu,
+          handleOnBlurEvent,
+          setDynamicPageData,
+          headingNavigationButton,
+          showItemFilterMenu,
+          handleNavbarMenuButtons,
+          handlePageHistory,
+        }}
+      >
         {this.props.children}
       </MyContext.Provider>
-    )
+    );
   }
 }
 
