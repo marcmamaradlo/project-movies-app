76a6cdaa62f0b7d90992563a2fc778b532953aaa
Marc Mamaradlo <marc.edwin.mamaradlo@gmail.com>
2024-01-02 01:49:13 +0800

    Fixes
    Fixed dead links for filipino movies and tv series.
    
    Fixed some issues with UI in smaller devices

--- src/components/video-player/Video-Player.js
+++ src/components/video-player/Video-Player.js
@@ -3,69 +3,71 @@ import { MyContext } from "../../context";
 import { useParams } from "react-router-dom";
 
 const VideoPlayer = () => {
+  const params = useParams();
+  const context = useContext(MyContext);
+  const serverButtonID = context.state.serverButtonID;
+  const vidsrcTo = `https://vidsrc.to/embed/movie/${params.id}`;
+  const vidsrcMe = `https://vidsrc.me/embed/movie?tmdb=${params.id}/`;
+  // const xtreamhubCom = `http://xtreamhub.com/video/${params.id}`;
+  const flixembed = `https://flixembed.com/video/${params.id}`;
 
-    const params = useParams();
-    const context = useContext(MyContext);
-    const serverButtonID = context.state.serverButtonID;
-    const vidsrcTo = `https://vidsrc.to/embed/movie/${params.id}`
-    const vidsrcMe = `https://vidsrc.me/embed/movie?tmdb=${params.id}/`
-    const xtreamhubCom = `http://xtreamhub.com/video/${params.id}`
-    // const xtreamhubCom = `http://fixembed.com/video/${params.id}`
-
-    const chooseServer = () => {
-        if (serverButtonID === 'server01') {
-            return <iframe
-                allowFullScreen
-                src={vidsrcTo}
-                id='video-player'
-                name='video-player'
-                title='video-player'
-            >
-                Video Player
-            </iframe>
-        }
-        if (serverButtonID === 'server02') {
-            return <iframe
-                allowFullScreen
-                src={vidsrcMe}
-                id='video-player'
-                name='video-player'
-                title='video-player'
-            >
-                Video Player
-            </iframe>
-        }
-        if (serverButtonID === 'server03') {
-            return <iframe
-                allowFullScreen
-                src={xtreamhubCom}
-                id='video-player'
-                name='video-player'
-                title='video-player'
-            >
-                Video Player
-            </iframe>
-        }
-        // if (serverButtonID === 'server04') {
-        //     return <iframe
-        //         allowFullScreen
-        //         src={vidsrcTo}
-        //         id='video-player'
-        //         name='video-player'
-        //         title='video-player'
-        //     >
-        //         Video Player
-        //     </iframe>
-        // }
-        else {
-            console.log('Sorry, video is not available in our servers.')
-        }
+  const chooseServer = () => {
+    if (serverButtonID === "server01") {
+      return (
+        <iframe
+          allowFullScreen
+          src={vidsrcTo}
+          id="video-player"
+          name="video-player"
+          title="video-player"
+        >
+          Video Player
+        </iframe>
+      );
     }
-    return (
-        <>
-            {chooseServer()}
-        </>
-    )
-}
+    if (serverButtonID === "server02") {
+      return (
+        <iframe
+          allowFullScreen
+          src={vidsrcMe}
+          id="video-player"
+          name="video-player"
+          title="video-player"
+        >
+          Video Player
+        </iframe>
+      );
+    }
+    if (serverButtonID === "server03") {
+      return (
+        <iframe
+          allowFullScreen
+          src={flixembed}
+          id="video-player"
+          name="video-player"
+          title="video-player"
+        >
+          Video Player
+        </iframe>
+      );
+    }
+    if (serverButtonID === "server04") {
+      return (
+        <iframe
+          allowFullScreen
+          src={flixembed}
+          id="video-player"
+          name="video-player"
+          title="video-player"
+        >
+          Video Player
+        </iframe>
+      );
+    } else {
+      console.log("Sorry, video is not available in our servers.");
+    }
+  };
+  return <>{chooseServer()}</>;
+};
 
 export default VideoPlayer;
