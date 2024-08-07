6a7f21bc7329f92975f293b9f546b424c273eb0f
Marc Mamaradlo <marc.edwin.mamaradlo@gmail.com>
2024-08-07 13:28:39 +0800

    Changes

--- src/components/video-player/Video-Player.js
+++ src/components/video-player/Video-Player.js
@@ -6,10 +6,12 @@ const VideoPlayer = () => {
   const params = useParams();
   const context = useContext(MyContext);
   const serverButtonID = context.state.serverButtonID;
-  const vidsrcTo = `https://vidsrc.to/embed/movie/${params.id}`;
+  // const vidsrcTo = `https://vidsrc.to/embed/movie/${params.id}`;
+  const vidsrcTo = `https://vidsrc.xyz/embed/movie/${params.id}`;
   const vidsrcMe = `https://vidsrc.me/embed/movie?tmdb=${params.id}/`;
   // const xtreamhubCom = `http://xtreamhub.com/video/${params.id}`;
-  const flixembed = `https://flixembed.com/video/${params.id}`;
+  // const flixembed = `https://flixembed.com/video/${params.id}`;
+  const flixembed = `https://streamug.com/video/${params.id}`;
 
   const chooseServer = () => {
     if (serverButtonID === "server01") {
